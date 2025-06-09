4b725f6239d652539e4d4e6462a9a68893e7a38c
Edan Liahovetsky <edan.liahovetsky@icloud.com>
2025-06-09 12:48:55 -0400

    quick init of robotstate

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.AbsoluteFieldDrive;
 import frc.robot.lib.input.XboxController;
 import frc.robot.subsystems.swerve.SwerveDrive;
@@ -19,16 +20,20 @@ public class RobotContainer {
     private final XboxController xboxDriver;
     private final XboxController xboxOperator;
 
+
     private RobotContainer() {
         this.xboxTester = new XboxController(1);
         this.xboxOperator = new XboxController(2);
         this.xboxDriver = new XboxController(3);
 
+        // we just initialize it here to save time during first reference in subsystems
+        RobotState.getInstance();
+
         swerveDrive = SwerveDrive.getInstance();
         swerveDrive.setDefaultCommand(new AbsoluteFieldDrive(xboxDriver));
     }
 
-    public Object getAutonomousCommand() {
+    public Command getAutonomousCommand() {
         return null;
     }
 }
